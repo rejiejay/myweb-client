cbf54c071cd41d67da411dd573ceddc5cf383059
Rejiejay <454766952@qq.com>
2018-10-24 18:40:12 +0800

    uupdate

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router, Route } from 'dva/router';
 // import { routerRedux } from 'dva/router';
+// app._store.dispatch(routerRedux.push('/mobile/preview/index'));
 import dynamic from 'dva/dynamic';
 
 import isMobile from './../utils/isMobile';
@@ -14,60 +15,46 @@ function RouterConfig({ history, app }) {
     dynamicModel.init(app);
     userModel.init(app);
 
+    // 手机端 首页
+    let mobileHome = dynamic({ app, component: () => import('./../views/mobile-new/index') });
+    // 电脑端 首页
+    let pcHome = dynamic({ app, component: () => import('./../views/computer/index') });
+
     return (
         <Router history={history}>
             <div className="router-content">
 
-                {/* 重定向的页面 */}
-                {browserRedirect(app)}
+                {/* 重定向的页面, 用于判断是否PC端还是手机端 */}
+                {isMobile() ? <Route exact path="/" component={mobileHome} /> : <Route exact path="/" component={dynamic({ app, component: () => import('./../views/computer/index') })} />}
 
 
                 {/* PC端口页面 */}
-                <Route path="/pc/index" component={dynamic({ app, component: () => import('./../views/computer/index') })} />
+                <Route path="/pc/index" component={pcHome} />
 
 
                 {/* 手机端首页 */}
-                <Route path="/mobile/index" component={dynamic({ app, component: () => import('./../views/mobile/index') })} />
-                {/* 手机端预览页 */}
+                <Route path="/mobile/index" component={mobileHome} />
+
+                
+                {/* 手机端 - 预览页 */}
                 <Route path="/mobile/preview/index" component={dynamic({ app, component: () => import('./../views/mobile/preview') })} />
+                {/* 手机端 - 预览 - 编辑页 */}
                 <Route path="/mobile/preview/edit" component={dynamic({ app, component: () => import('./../views/mobile/preview-edit') })} />
+                {/* 手机端 - 动态预览 */}
                 <Route path="/mobile/dynamic/preview" component={dynamic({ app, component: () => import('./../views/mobile/dynamic/preview') })} />
+                {/* 手机端 - 动态 分组 */}
                 <Route path="/mobile/dynamic/group" component={dynamic({ app, component: () => import('./../views/mobile/dynamic/group') })} />
+                {/* 手机端 - 动态 分组 列表 */}
                 <Route path="/mobile/dynamic/group-list" component={dynamic({ app, component: () => import('./../views/mobile/dynamic/group-list') })} />
+                {/* 手机端 - 动态 分组 列表 编辑 */}
                 <Route path="/mobile/dynamic/edit" component={dynamic({ app, component: () => import('./../views/mobile/dynamic/edit') })} />
+
 
                 {/* 登录页面 */}
                 <Route path="/user/login" component={dynamic({ app, component: () => import('./../views/user/login') })} />
             </div>
         </Router>
     );
-}
-
-/**
- * 用于判断是否PC端还是手机端的方法
- */
-function browserRedirect(app) {
-    /**
-     * 判断一下是否手机端
-     */
-    if (isMobile()) {
-        /**
-         * 判断是否有参数, 
-         * 有参数说明一定是进行操作的，所以跳到在预览页面
-         * 因为预览页面就是根据ID进行查询
-         */
-        // if (window.location.href.indexOf('?') === -1 && window.location.hash !== '#/mobile/preview/index') {
-        //     app._store.dispatch(routerRedux.push('/mobile/preview/index'));
-        // }
-        return (
-            <Route exact path="/" component={dynamic({ app, component: () => import('./../views/mobile/index') })} />
-        )
-    } else {
-
-        return (
-            <Route exact path="/" component={dynamic({ app, component: () => import('./../views/computer/index') })} />
-        )
-    }
 }
 
 export default RouterConfig;
