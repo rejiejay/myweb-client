b5f95cd2dfcd119f4d39eac96a5450f1534677ae
Rejiejay <454766952@qq.com>
2018-12-20 17:18:37 +0800

    update resume

--- src/views/mobile/index.js
+++ src/views/mobile/index.js
@@ -404,45 +404,61 @@ class mobile extends Component {
      * 渲染 简历
      */
     renderReusemeList() {
+        /**
+         * 跳转到 简历列表
+         */
+        let jumpToReusemeList = () => window.location.href = `./#/reuseme/list`;
+
         // 简历的数据
         // 这里展示 7 条数据即可
         let reusemeList = [
             {
-                imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/page-assets/picture/very_sorry.png',
+                id: 'flgx', // 数据唯一标识
+                imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/reuseme/flgx/banner.png',
                 title: '金车管家',
                 content: '“金车管家”是使用Vue框架，以及Java Spring Boot服务的前后端分离项目，面向“保险销售人员、保险代理人员或其他车务、保险相关从业人员”的展业工具，帮助其管理现有客户、拓展新客户，创造客户接触机会，给客户提供加油、洗车、保养等高品质优惠服务，给业务经理分成。',
             }, {
-                imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/page-assets/picture/very_sorry.png',
+                id: 'picccarwash',
+                imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/reuseme/picc-car-wash/banner.png',
                 title: '深圳人保财险PICC洗车项目',
                 content: '人保洗车项目是使用Play框架编写的全栈的Java Web的项目, 与另一套Java服务集成后台管理端，可随时导入新增门店，客户端全深圳城市的人保洗车门店情况；通过目的门店站搜索、筛选，微信定位、可以快速定位到要查询的门店，一键导航洗车门店。',
             }, {
-                imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/page-assets/picture/very_sorry.png',
+                id: 'pingandealere',
+                imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/reuseme/pingan-dealer-E/banner.png',
                 title: '深圳平安财险车商小程序',
                 content: '车商小程序是在平安科技体系下的一个小程序项目，汇集多家汽车经销商，提供维修保养、新车试驾、保险询价等线上服务。',
             }, {
-                imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/page-assets/picture/very_sorry.png',
+                id: 'picccharger',
+                imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/reuseme/picc-charger/banner.png',
                 title: '充电桩新能源项目',
-                content: '充电桩是使用Vue框架，后台是Java和C#提供服务的前后端分离项目，该项目对接特来电等多家公司的服务。',
+                content: '充电桩是使用Vue框架，后台是Java和C#提供服务的前后端分离项目，该项目对接中国人保,中石化,特来电等多家公司的服务。可以快速定位到要查询的充电桩，一键启动充电。',
             }, {
-                imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/page-assets/picture/very_sorry.png',
+                id: 'ycpd',
+                imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/reuseme/ycpd/banner.png',
                 title: '养车频道车主端',
                 content: '养车频道车主端是使用Vue前端框架, 后台为C#以及JAVA的平台项目，平台近四百家商户，以及几十家合作伙伴，为超过10万名车主提供过服务。',
             }, {
-                imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/page-assets/picture/very_sorry.png',
+                id: 'divingtime',
+                imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/reuseme/divingtime/banner.png',
                 title: '潜游时光商城',
                 content: '潜游时光商城是使用了jquery与React Dva多种前端技术，后台为Java服务前后端分离的电商网站。网站兼容PC端以及移动端。',
             }, {
+                id: 'divingtimeuserinfor',
                 imgsrc: 'https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/page-assets/picture/very_sorry.png',
                 title: '潜游时光用户信息收集',
                 content: '潜游时光用户信息收集是承担了业务流程优化的一个项目。项目使得客服人员工作量大幅度下降。优化用户体验，同时为网站积累用户以及数据量便于以后拓展使用。',
             },
-        ]
+        ];
+
+        /**
+         * 跳转到作品详情
+         */
+        let jumpToReusemeRetails = id => window.location.href = `./?id=${id}#/reuseme/details`;
 
         return this.state.navBarStatus === 'resume' ? (
             <div className="mobile-list-resume">
 
-                {/* 标题列 为了 新增 */}
-                <div className="list-resume-title flex-start">
+                <div className="list-resume-title flex-start" onClick={jumpToReusemeList}>
                     <div className="resume-title-lable flex-rest">简历作品</div>
                     <div className="resume-title-add  flex-start-center">
                         <span>更多</span>
@@ -454,7 +470,7 @@ class mobile extends Component {
                 
                 {reusemeList.map((val, key) => (
                     <div className="mobile-list-item" key={key}>
-                        <div className="list-item-container">
+                        <div className="list-item-container" onClick={() => jumpToReusemeRetails(val.id)}>
 
                             {/* 图片 */}
                             <div className="list-item-img" 
--- src/views/reuseme/details.js
+++ src/views/reuseme/details.js
@@ -1,15 +1,50 @@
 // 框架类
 import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
 // 组件类
 import ReusemeHeader from './reuseme-header';
+import loadPageVar from './../../utils/loadPageVar';
 // 样式类
 import './details.scss';
+// 初始化
+let clientWidth = document.body.offsetWidth || document.documentElement.clientWidth || window.innerWidth;
+let clientHeight = document.body.offsetHeight || document.documentElement.clientHeight || window.innerHeight;
+// 简历键值对数据
+let reusemekeyvaldata = {
+
+}
 
 class details extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            list: [
+                // {
+                //     title: '',
+                //     content: '',
+                // },
+            ],
         };
+    }
+
+    /**
+     * 组件加载完毕之后立即执行
+     */
+    componentDidMount() {
+        // 初始化简历数据的“键(key)”值
+        let datakeyName = loadPageVar('id');
+
+        if (datakeyName) {
+            // 初始化页面数据
+            let pageData = reusemekeyvaldata[datakeyName];
+    
+            // 复制进 state 里
+            if (pageData) {
+                this.setState({
+                    list: pageData
+                });
+            }
+        }
     }
 
     render() {
@@ -18,7 +53,18 @@ class details extends Component {
             <React.Fragment>
                 <ReusemeHeader title="作品详情" />
 
-                <div className="resume-details">
+                <div className="resume-details" style={{minHeight: `${clientHeight - 50}px`}}>
+                
+                    {this.state.list.map((item, key) => (
+                        <div className="resume-details-item" key={key}>
+                            <div className="details-item-container">
+                                <div className="details-item-title">{item.title}</div>
+                                <div className="details-item-main ReactMarkdown">
+                                    <ReactMarkdown source={item.content} />
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </React.Fragment>
         )
