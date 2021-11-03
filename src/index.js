9c035c2a41507bdead7c61df0d94e731f7d67f0c
rejiejay <454766952@qq.com>
2021-11-03 20:35:27 +0800

    update version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-// 框架类
 import React from 'react';
 import ReactDOM from 'react-dom';
-// 自定义组件类
-import RouterConfig from './routes/index';
-import modelsInit from './models/init';
-// css 样式类
-import './index.scss';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// 渲染 react 组件
-ReactDOM.render(<RouterConfig />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// 初始化 顶层数据
-modelsInit.all();
-
-// 清空 '预加载' 样式
-document.getElementById('loading').innerHTML = '';
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
